a7ee65c03f9719aed6f29a74df7b02a2a3936084
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2020-04-24 17:54:33 -0400

    Clean up comments from previous code

--- packages/web/src/Main.elm
+++ packages/web/src/Main.elm
@@ -1,11 +1,5 @@
 module Main exposing (..)
 
--- Press buttons to increment and decrement a counter.
---
--- Read how it works:
---   https://guide.elm-lang.org/architecture/buttons.html
---
-
 import Browser
 import Html exposing (Html, div, table, tr, td, text)
 import Html.Attributes exposing (classList)
